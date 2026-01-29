9a0080b613ab0fc822fd288a6061b3fa74ae962e
hamzaxbella <hamza.bella.36@edu.uiz.ac.ma>
2026-01-29 14:37:47 +0100

    appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.zyllux.fastfood",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
